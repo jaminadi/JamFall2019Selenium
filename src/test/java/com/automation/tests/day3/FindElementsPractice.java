c64baa36ea54564fdb071c3655035c7fb69af0f6
jaminadi <jama.nadyrbek@gmail.com>
2022-02-15 09:53:23 -0600

    Basic navigation class

--- src/test/java/com/automation/tests/day3/FindElementsPractice.java
+++ src/test/java/com/automation/tests/day3/FindElementsPractice.java
@@ -1,16 +1,39 @@
 package com.automation.tests.day3;
 
+import com.automation.utilities.DriverFactory;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class FindElementsPractice {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
-        WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
+        // WebDriverManager.chromedriver().setup();
+        // WebDriver driver = new ChromeDriver();
 
+        WebDriver driver = DriverFactory.createDriver("chrome");
+        driver.get("http://practice.cybertekschool.com/sign_up");
+        WebElement fullName = driver.findElement(By.name("full_name"));
+        fullName.sendKeys("Mister Twister");
+
+        Thread.sleep(2000);
+
+        WebElement email = driver.findElement(By.name("email"));
+        email.sendKeys("sdet@cybertek.com"); // to enter email in the email section
+
+        Thread.sleep(2000);
+
+        WebElement signUp = driver.findElement(By.name("wooden_spoon"));
+        //when you see type="submit", you can use submit() instead of click()
+        //it makes sense to use it when click() method does not work
+        signUp.submit(); // to simmulate clicking on Signup button
+
+        Thread.sleep(2000);
+
+        driver.quit();//to close everything
 
     }
 
