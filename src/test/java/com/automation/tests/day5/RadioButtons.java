e1ead48a38d3098338dd4f6d86f84451628231aa
jaminadi <jama.nadyrbek@gmail.com>
2022-02-16 19:28:28 -0600

    Clicking on radio buttons

--- src/test/java/com/automation/tests/day5/RadioButtons.java
+++ src/test/java/com/automation/tests/day5/RadioButtons.java
@@ -1,18 +1,39 @@
 package com.automation.tests.day5;
 
+import com.automation.utilities.BrowserUtils;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
 
 public class RadioButtons {
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
 
         driver.get("http://practice.cybertekschool.com/radio_buttons");
         driver.manage().window().maximize();
-        Thread.sleep(2000);
+
+        BrowserUtils.wait(2);
+        //<input type="radio>
+        List<WebElement> radioButtons = driver.findElements(By.tagName("input"));
+
+        for (WebElement radioButton : radioButtons) { // we take each radiobutton
+
+            String id = radioButton.getAttribute("id");
+            //isEnabled is to check if disabled button can be clicked (returns boolean)
+            if (radioButton.isEnabled()) {
+                radioButton.click(); // and click on each of them
+                System.out.println("Clicked on :: " + id);
+                BrowserUtils.wait(1);
+            } else {
+                System.out.println("Button is disabled, not clicked :: " + id);
+            }
+        }
 
         driver.quit();
 
