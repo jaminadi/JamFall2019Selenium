f5a2e57eb858e4bed3dc6764c870b4743a8ab79e
jaminadi <jama.nadyrbek@gmail.com>
2022-02-16 18:49:20 -0600

    Exception for Thread.sleep

---
+++ src/test/java/com/automation/tests/day5/RadioButtons.java
@@ -0,0 +1,21 @@
+package com.automation.tests.day5;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class RadioButtons {
+
+    public static void main(String[] args) throws Exception {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("http://practice.cybertekschool.com/radio_buttons");
+        driver.manage().window().maximize();
+        Thread.sleep(2000);
+
+        driver.quit();
+
+
+    }
+}
