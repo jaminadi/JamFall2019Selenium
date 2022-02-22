9cc6f0166381b61e58bec13fd09342f9d8c30d18
jaminadi <jama.nadyrbek@gmail.com>
2022-02-22 09:53:51 -0600

    Drop-downs practice / Printing out all options from the dropdown link

--- src/test/java/com/automation/tests/day6/NoSelectDropDown.java
+++ src/test/java/com/automation/tests/day6/NoSelectDropDown.java
@@ -4,6 +4,9 @@ import com.automation.utilities.BrowserUtils;
 import com.automation.utilities.DriverFactory;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
 
 public class NoSelectDropDown {
 
@@ -15,6 +18,13 @@ public class NoSelectDropDown {
 
         driver.findElement(By.id("dropdownMenuLink")).click();//to expand dropdown
         BrowserUtils.wait(2);
+
+        //print out all links from the dropdown link
+        List<WebElement> allLinks = driver.findElements(By.className("dropdown-item"));
+        for (WebElement eachLink : allLinks) {
+            System.out.println(eachLink.getText() + " : " + eachLink.getAttribute("href"));
+        }
+
         driver.findElement(By.linkText("Amazon")).click();//click on selected option
 
         BrowserUtils.wait(3);
