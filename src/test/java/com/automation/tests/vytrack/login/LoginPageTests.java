e0e469f2976d3a126df2acc8ce9fbc17855e7db8
jaminadi <jama.nadyrbek@gmail.com>
2022-02-28 09:34:03 -0600

    VyTrack Login Functionality test

--- src/test/java/com/automation/tests/vytrack/login/LoginPageTests.java
+++ src/test/java/com/automation/tests/vytrack/login/LoginPageTests.java
@@ -1,20 +1,42 @@
 package com.automation.tests.vytrack.login;
 
+import com.automation.utilities.BrowserUtils;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 //static import of all Assertions
 import static org.testng.Assert.*;
+//static import allows you to use staticc methods without specifying class name
 
 public class LoginPageTests {
     private WebDriver driver;
     private String URL = "https://qa2.vytrack.com/user/login";
+    private By usernameBy = By.id("prependedInput"); // By is a class
+    private By passwordBy = By.id("prependedInput2");
+    // > in css selector means same thing as / in xpath - direct child
+    private By warningMessageBy = By.cssSelector("[class='alert alert-error'] > div");
+
+
+    @Test(description = "verify that warning message displays when user enters invalid username")
+    public void invalidUsername() {
+        driver.findElement(usernameBy).sendKeys("invalidusername");
+        driver.findElement(passwordBy).sendKeys("UserUser123", Keys.ENTER);
+        BrowserUtils.wait(3);
+        WebElement warningElement = driver.findElement(warningMessageBy);
+        assertTrue(warningElement.isDisplayed()); // if warning message is not visible, test will fail
+
+    }
 
     @BeforeMethod
     public void setup() {
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
+        driver.get(URL);
         driver.manage().window().maximize();
     }
 
