964093893dbc9ecb009699a0f7e5d53f1b10ace4
jaminadi <jama.nadyrbek@gmail.com>
2022-03-02 13:43:45 -0600

    JavascriptExecutor/Scroll until certain point

--- src/test/java/com/automation/tests/day9/JSExecutor.java
+++ src/test/java/com/automation/tests/day9/JSExecutor.java
@@ -3,8 +3,10 @@ package com.automation.tests.day9;
 import com.automation.utilities.BrowserUtils;
 import com.automation.utilities.DriverFactory;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterMethod;
@@ -28,9 +30,21 @@ public class JSExecutor {
         for (int i = 0; i < 10; i++) {
             driver.executeScript("window.scrollBy(0, 250)");
             BrowserUtils.wait(3);
-
         }
     }
+
+    //there is a method that allows you to scroll until certain point
+    @Test
+    public void scrollToElementTest() {
+        driver.get("http://practice.cybertekschool.com/");
+        driver.manage().window().maximize();
+        BrowserUtils.wait(3);
+        WebElement link = driver.findElement(By.linkText("Cybertek School"));
+        driver.executeScript("arguments[0].scrollIntoView(true)", link);
+        //scroll untill <link> becomes visible
+
+    }
+
 
     @BeforeMethod
     public void setup() {
