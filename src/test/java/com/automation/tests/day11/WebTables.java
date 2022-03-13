bd91ec9bfaaf2385e7ac1a187e059f5a576f00d9
jaminadi <jama.nadyrbek@gmail.com>
2022-03-13 18:45:04 -0500

    Utilities - Get text from WebElements

---
+++ src/test/java/com/automation/tests/day11/WebTables.java
@@ -0,0 +1,45 @@
+package com.automation.tests.day11;
+
+import com.automation.utilities.BrowserUtils;
+import com.automation.utilities.DriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class WebTables {
+
+    private WebDriver driver;
+
+    @BeforeMethod
+    public void setup() {
+        driver = DriverFactory.createDriver("chrome");
+        driver.get("http://practice.cybertekschool.com/tables");
+        driver.manage().window().maximize();
+    }
+
+    @Test
+    public void getColumnNames() {
+        // th - represents table header cells
+        List<String> expected = Arrays.asList("Last Name", "First Name", "Email", "Due", "Web Site", "Action");
+        List<WebElement> columnNames = driver.findElements(By.xpath("//table[1]//th"));
+
+        for (WebElement columnName : columnNames) {
+            System.out.println(columnName.getText());
+        }
+    }
+
+
+    @AfterMethod
+    public void tearDown() {
+        BrowserUtils.wait(2);
+        driver.quit();
+    }
+
+
+}
