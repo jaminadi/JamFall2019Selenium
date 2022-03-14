d15f104b603ea6d33cc073f4c0c06776d4435577
jaminadi <jama.nadyrbek@gmail.com>
2022-03-14 18:37:34 -0500

    Synchronization - Implicit wait demo

--- src/test/java/com/automation/tests/day12/ImplicitWait.java
+++ src/test/java/com/automation/tests/day12/ImplicitWait.java
@@ -3,6 +3,7 @@ package com.automation.tests.day12;
 import com.automation.utilities.DriverFactory;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -16,6 +17,7 @@ public class ImplicitWait {
     public void setup() {
         driver = DriverFactory.createDriver("chrome");
         driver.get("http://practice.cybertekschool.com/dynamic_loading/2");
+        //this is how we apply implicit wait
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     }
 
@@ -27,6 +29,9 @@ public class ImplicitWait {
     @Test
     public void waitTest() {
         driver.findElement(By.tagName("button")).click();
+        WebElement finishElement = driver.findElement(By.id("finish"));
+        System.out.println(finishElement.getText());
+
     }
 
 
