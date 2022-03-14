0ef5f86a9dbb7b2326bc0fd7a9d5aa4d5257874e
jaminadi <jama.nadyrbek@gmail.com>
2022-03-14 08:29:22 -0500

    Web Tables - Verifying row counts

--- src/test/java/com/automation/tests/day11/WebTables.java
+++ src/test/java/com/automation/tests/day11/WebTables.java
@@ -37,6 +37,14 @@ public class WebTables {
         Assert.assertEquals(BrowserUtils.getTextFromWebElements(columnNames), expected);
     }
 
+    @Test
+    public void verifyRowCount() {
+        // //tbody//tr - to get all rows from table body, excluding table header
+        List<WebElement> rows = driver.findElements(By.xpath("//table[1]//tbody//tr"));
+        //if we will get the size of this collection, it automatically equals to number of elements
+        Assert.assertEquals(rows.size(), 4);
+    }
+
 
     @AfterMethod
     public void tearDown() {
