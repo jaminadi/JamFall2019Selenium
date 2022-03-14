f07aea873dd4c74e7fcfaa5d3c4ff64b336a4724
jaminadi <jama.nadyrbek@gmail.com>
2022-03-14 08:19:30 -0500

    Web Tables - Getting column names

--- src/test/java/com/automation/tests/day11/WebTables.java
+++ src/test/java/com/automation/tests/day11/WebTables.java
@@ -5,6 +5,7 @@ import com.automation.utilities.DriverFactory;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -32,6 +33,7 @@ public class WebTables {
         for (WebElement columnName : columnNames) {
             System.out.println(columnName.getText());
         }
+        Assert.assertEquals(BrowserUtils.getTextFromWebElements(columnNames), expected);
     }
 
 
