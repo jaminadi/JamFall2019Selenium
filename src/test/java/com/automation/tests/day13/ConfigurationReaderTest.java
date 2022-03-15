ceb9ad3ee40c24bbeec60d7bcf8d035dfdb70e44
jaminadi <jama.nadyrbek@gmail.com>
2022-03-15 17:59:06 -0500

    Utilities - configuration.properties file

--- src/test/java/com/automation/tests/day13/ConfigurationReaderTest.java
+++ src/test/java/com/automation/tests/day13/ConfigurationReaderTest.java
@@ -1,6 +1,16 @@
 package com.automation.tests.day13;
 
+import com.automation.utilities.ConfigurationReader;
+import org.testng.annotations.Test;
+
+import java.io.ObjectInputFilter;
+
 public class ConfigurationReaderTest {
 
+    @Test
+    public void readProperties() {
+        String browser = ConfigurationReader.getProperty("browser");
+        System.out.println(browser);
+    }
 
 }
