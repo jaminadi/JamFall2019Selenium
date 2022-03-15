d77dfa2f5fbbbb8f0c1e739b5bde4e0d030ab6c4
jaminadi <jama.nadyrbek@gmail.com>
2022-03-15 09:16:02 -0500

    Synchronization - Explicit wait demo (Wait until button becomes visible and the loadingoverlay screen becomes invisible)

--- src/test/java/com/automation/tests/day12/ExplicitWait.java
+++ src/test/java/com/automation/tests/day12/ExplicitWait.java
@@ -58,6 +58,28 @@ public class ExplicitWait {
 
     }
 
+    @Test
+    public void elementToBeClickableTest() {
+        driver.get("http://practice.cybertekschool.com/dynamic_loading/5");
+
+        WebDriverWait wait = new WebDriverWait(driver, 15);
+
+        WebElement username = driver.findElement(By.name("username"));
+        WebElement password = driver.findElement(By.name("password"));
+        WebElement submitBtn = driver.findElement(By.cssSelector("button[type='submit']"));
+
+        wait.until(ExpectedConditions.invisibilityOfElementLocated(By.className("loadingoverlay")));
+        wait.until(ExpectedConditions.elementToBeClickable(submitBtn));
+
+        username.sendKeys("tomsmith");
+        password.sendKeys("SuperSecretPassword");
+        submitBtn.click();
+
+        String expected = "Welcome to the Secure Area. When you are done click logout below.";
+        String actual = driver.findElement(By.className("subheader")).getText();
+        Assert.assertEquals(actual, expected);
+    }
+
     @AfterMethod
     public void tearDown() {
         driver.quit();
