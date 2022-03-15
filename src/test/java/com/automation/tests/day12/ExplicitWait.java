f04d544210dcad6822a5ce160d120435c4c897a6
jaminadi <jama.nadyrbek@gmail.com>
2022-03-15 08:55:26 -0500

    Synchronization - Explicit wait demo (Wait until button becomes visible)

--- src/test/java/com/automation/tests/day12/ExplicitWait.java
+++ src/test/java/com/automation/tests/day12/ExplicitWait.java
@@ -1,9 +1,12 @@
 package com.automation.tests.day12;
 
 import com.automation.utilities.DriverFactory;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -27,6 +30,29 @@ public class ExplicitWait {
 
         driver.navigate().to("https://amazon.com");
         wait.until(ExpectedConditions.titleContains("Amazon"));
+
+    }
+
+    @Test
+    public void waitForVisibility() {
+        driver.get("http://practice.cybertekschool.com/dynamic_loading/1");
+
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        driver.findElement(By.tagName("button")).click();
+
+        WebElement username = driver.findElement(By.name("username"));
+        WebElement password = driver.findElement(By.name("password"));
+        WebElement submitBtn = driver.findElement(By.cssSelector("button[type='submit']"));
+
+        wait.until(ExpectedConditions.visibilityOf(username)).sendKeys("tomsmith");
+        wait.until(ExpectedConditions.visibilityOf(password)).sendKeys("SuperSecretPassword");
+
+        wait.until(ExpectedConditions.visibilityOf(submitBtn)).click();
+        //wait.until(ExpectedConditions.elementToBeClickable(submitBtn)).click();
+
+        String expected = "Welcome to the Secure Area. When you are done click logout below.";
+        String actual = driver.findElement(By.className("subheader")).getText();
+        Assert.assertEquals(actual, expected);
 
     }
 
