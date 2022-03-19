d0487de71497741964b6a85693df0485db906498
jaminadi <jama.nadyrbek@gmail.com>
2022-03-19 13:30:08 -0500

    Page Class - Login functionality

---
+++ src/test/java/com/automation/pages/LoginPage.java
@@ -0,0 +1,55 @@
+package com.automation.pages;
+
+import com.automation.utilities.ConfigurationReader;
+import com.automation.utilities.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPage {
+
+    @FindBy(id = "prependedInput") //we put annotation FindBy on top of instance var and specify locator type
+    public WebElement username; //this is reference for username WebElement
+    //or  public WebElement username2 = Driver.getDriver().findElement(By.id("perpendedInput"));
+
+    @FindBy(id = "prependedInput2")
+    public WebElement password;
+
+    @FindBy(id = "_submit")
+    public WebElement login;
+
+    @FindBy(linkText = "Forgot your password?")
+    public WebElement forgotPassword;
+
+    public LoginPage() { //constructor
+        //to connect our WebDriver, page class and page factory
+        //PageFactory - used for using @FindBy annotations
+        //PageFactory - makes the process of finding elements much easier
+        //or LoginPage.class instead of this
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    /**
+     * Method to login, version 1
+     * Login as a specific user
+     *
+     * @param usernameValue
+     * @param passwordValue
+     */
+    public void login(String usernameValue, String passwordValue) { //this method is overloaded
+        username.sendKeys(usernameValue);
+        password.sendKeys(passwordValue, Keys.ENTER);
+    }
+
+    /**
+     * Method to login, version 2
+     * Login as a default user
+     * Credentials will be retrieved from the configuration.properties file
+     */
+    public void login() {
+        username.sendKeys(ConfigurationReader.getProperty("store_manager"));
+        password.sendKeys(ConfigurationReader.getProperty("password"), Keys.ENTER);
+    }
+}
---
+++ src/test/java/com/automation/tests/vytrack/TestBase.java
@@ -0,0 +1,29 @@
+package com.automation.tests.vytrack;
+
+import com.automation.utilities.ConfigurationReader;
+import com.automation.utilities.Driver;
+import org.openqa.selenium.interactions.Action;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public abstract class TestBase { //this class is supposed to act like design/setup, so we should think of its visibility
+    protected WebDriverWait wait; //protected because we need it to be visible within the package
+    //and within the subclass, regardless of subclass location
+    protected Actions actions;
+
+    @BeforeMethod
+    public void setup() {
+        String URL = ConfigurationReader.getProperty("qa1");
+        Driver.getDriver().get(URL);
+        Driver.getDriver().manage().window().maximize();
+        wait = new WebDriverWait(Driver.getDriver(), 15);
+        actions = new Actions(Driver.getDriver());
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        Driver.closeDriver();
+    }
+}
