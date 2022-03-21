14f8b82c6ab0271ac5b8a5122cf832d195b78770
jaminadi <jama.nadyrbek@gmail.com>
2022-03-21 08:42:09 -0500

    Abstract page class - Navigate to tabs and module names on VyTrack

--- src/test/java/com/automation/tests/vytrack/AbstractTestBase.java
+++ src/test/java/com/automation/tests/vytrack/AbstractTestBase.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
-public abstract class TestBase { //this class is supposed to act like design/setup, so we should think of its visibility
+public abstract class AbstractTestBase { //this class is supposed to act like design/setup, so we should think of its visibility
     protected WebDriverWait wait; //protected because we need it to be visible within the package
     //and within the subclass, regardless of subclass location
     protected Actions actions;
