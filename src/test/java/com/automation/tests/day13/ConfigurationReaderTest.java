eb35eaa0153401c447ce5d48b576095f7337435c
jaminadi <jama.nadyrbek@gmail.com>
2022-03-29 17:34:52 -0500

    day25 - Excel IO (Read excel file test)

--- src/test/java/com/automation/tests/day13/ConfigurationReaderTest.java
+++ src/test/java/com/automation/tests/day13/ConfigurationReaderTest.java
@@ -3,7 +3,6 @@ package com.automation.tests.day13;
 import com.automation.utilities.ConfigurationReader;
 import org.testng.annotations.Test;
 
-import java.io.ObjectInputFilter;
 
 public class ConfigurationReaderTest {
 
--- src/test/java/com/automation/tests/day25_excel_io/ReadDataFromExcel.java
+++ src/test/java/com/automation/tests/day25_excel_io/ReadDataFromExcel.java
@@ -1,11 +1,32 @@
 package com.automation.tests.day25_excel_io;
 
+import org.apache.poi.ss.usermodel.*;
 import org.testng.annotations.Test;
+
+import java.io.File;
 
 public class ReadDataFromExcel {
 
     @Test
-    public void readExcelFileTest() {
+    public void readExcelFileTest() throws Exception {
+        //we need to get the file as an object
+        File file = new File("VytrackTestUsers.xlsx");
+        //object that represents excel file
+        Workbook workbook = WorkbookFactory.create(file);
+        //get QA1-short
+        Sheet workSheet = workbook.getSheet("QA1-short");
+        //get 1st row
+        Row firstRow = workSheet.getRow(0);
+        //get 1st cell
+        Cell firstCell = firstRow.getCell(0);
+        //get string value
+        String value = firstCell.getStringCellValue();
+
+        String secondCellValue = firstRow.getCell(1).getStringCellValue();
+
+        System.out.println(value); // execute
+        System.out.println(secondCellValue);// username
+
 
     }
 }
